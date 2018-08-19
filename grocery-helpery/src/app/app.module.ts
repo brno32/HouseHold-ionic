e7975486711795f347daeaf4197cd7abfa3108b8
Alex Drozd <drozdster@gmail.com>
2018-08-19 11:16:50 -0500

    adds firebase

--- grocery-helpery/src/app/app.module.ts
+++ grocery-helpery/src/app/app.module.ts
@@ -8,6 +8,22 @@ import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { LoginPage } from '../pages/login/login';
 
+import firebase from 'firebase';
+
+var config = {
+    apiKey: "AIzaSyBIKDvdgxOBt2HL9wlzPVrDHONjbeOevgc",
+    authDomain: "grocery-app-d1fc1.firebaseapp.com",
+    databaseURL: "https://grocery-app-d1fc1.firebaseio.com",
+    projectId: "grocery-app-d1fc1",
+    storageBucket: "grocery-app-d1fc1.appspot.com",
+    messagingSenderId: "870514586428"
+};
+
+firebase.initializeApp(config);
+firebase.firestore().settings({
+  timestampsInSnapshots: true,
+})
+
 @NgModule({
   declarations: [
     MyApp,
