b71cb3d1f76afdcba83c535185fdedf5ddf27ef8
Alex Drozd <drozdster@gmail.com>
2018-08-19 12:01:46 -0500

    adds log out

--- grocery-helpery/src/pages/home/home.ts
+++ grocery-helpery/src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController, ToastController } from 'ionic-angular';
+import { LoginPage } from '../login/login';
+
+import firebase from 'firebase';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,20 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private loadingCtrl: LoadingController, private toastCtrl: ToastController) {
 
+  }
+
+  logout() {
+    firebase.auth().signOut().then(() => {
+
+      let toast = this.toastCtrl.create({
+        message: "Logged out",
+        duration: 3000,
+      }).present();
+
+      this.navCtrl.setRoot(LoginPage);
+    });
   }
 
 }
