1c4280f3c91b1cb33c9d220069cf9f2704a43f7b
Alex Drozd <drozdster@gmail.com>
2018-08-19 22:48:52 -0500

    lays out feed UI and changes placeholder text for sign in fields

--- grocery-helpery/src/app/app.component.ts
+++ grocery-helpery/src/app/app.component.ts
@@ -3,14 +3,14 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-//import { LoginPage } from '../pages/login/login';
-import { FeedPage } from '../pages/feed/feed';
+import { LoginPage } from '../pages/login/login';
+// import { FeedPage } from '../pages/feed/feed';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = FeedPage;
+  rootPage:any = LoginPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
